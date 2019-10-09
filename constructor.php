b8cbcbc498683ecb7ac12014efe9bf8b24ae1f7a
Evi Silvia <evi_173040050@mail.unpas.ac.id>
2019-10-10 00:24:15 +0700

    menambahkan file constructor

---
+++ constructor.php
@@ -0,0 +1,53 @@
+<?php 
+
+//jualan produk
+//komik
+//game
+
+class Produk{
+	public $judul ,
+		   $penulis ,
+		   $penerbit ,
+		   $harga ; 
+
+	public function __construct($judul = "judul",$penulis ="penulis",$penerbit = "penerbit",$harga =0){
+		$this->judul =$judul;
+		$this->penulis =$penulis;
+		$this->penerbit =$penerbit;
+		$this->harga =$harga;
+		
+	}
+
+
+
+   public function getLabel(){
+   	return "$this->penulis, $this->penerbit";
+   }
+}
+
+// $produk1 =new Produk();
+// $produk1->judul = "Naruto";
+
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProferty = "haha";
+// var_dump($produk2);
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump",30000);
+
+$produk2 = new Produk("Uncharted", "Neil Druckmann","Sony Computer", 250000);
+$produk3 = new Produk("Dragon Ball");
+
+
+
+
+//var_dump($produk3);
+echo "Komik :" . $produk3->getLabel(); 
+echo "<br>";
+echo "Game :" . $produk3->getLabel(); 
+var_dump($produk3);
+
+
+ ?>
